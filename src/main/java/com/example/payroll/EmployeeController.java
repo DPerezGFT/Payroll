fa8078ff316d159d70972b73f55e81679e12d649
Daniel Pérez <dlpcgft.com>
2022-05-24 10:17:34 +0200

    Quitado el CrossOrigin y Añadido el OrderNotFoundAdvice

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -13,7 +13,6 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@CrossOrigin
 class EmployeeController {
 
     private final EmployeeRepository repository;
